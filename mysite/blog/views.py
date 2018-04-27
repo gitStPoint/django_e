efadd7d7d744c227679a351196d21e5198095ff8
BAI-PC\Bai7 <jjls45@163.com>
2018-04-27 17:57:54 +0800

    url link

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
 # Create your views here.
@@ -7,4 +7,10 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, '../templates/blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk1):
+    post = get_object_or_404(Post, pk=pk1)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 
